c19f2f0f8576d65cbdf92c0075d3b70cf541169c
Md. Hasibuzzaman <45512066+hasib2019@users.noreply.github.com>
2022-04-28 15:49:01 +0600

    new

--- resources/views/common/edit_from.blade.php
+++ resources/views/common/edit_from.blade.php
@@ -307,10 +307,10 @@
                   <button class="btn btn-success" type="button"><i class="fldemo glyphicon glyphicon-plus"></i>Add</button>
                 </div>
               </div>
-              <div class="clone hide">
+              <div class="clone hide" hidden>
                 <div class="hdtuto control-group lst input-group" style="margin-top:10px">
                   <input type="file" name="filenames[]" class="myfrm form-control">
-                  <input type="date" name="filedate[]" hidden class="myfrm form-control">
+                  <input type="date" name="filedate[]" class="myfrm form-control">
                   <div class="input-group-btn"> 
                     <button class="btn btn-danger" type="button"><i class="fldemo glyphicon glyphicon-remove"></i> Remove</button>
                   </div>
